4c3276c6e2a8f7ec0ee96ad96bcf12b38d4e55fe
NihadRasulzada <nihadrs2005@gmail.com>
2025-01-22 00:01:26 +0400

    AddGenericRepository

---
+++ Infrastructure/MiniEcommerceServer.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MiniEcommerceServer.Application.Repositories;
+using MiniEcommerceServer.Domain.Entities.Common;
+using MiniEcommerceServer.Persistence.Contexts;
+
+namespace MiniEcommerceServer.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly ECommerceAPIContext _context;
+        public ReadRepository(ECommerceAPIContext context) => _context = context;
+
+        public DbSet<T> Table => _context.Set<T>();
+        public IQueryable<T> GetAll() => Table;
+        public async Task<T> GetByIdAsync(string id) => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method) => await Table.FirstOrDefaultAsync(method);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method) => Table.Where(method);
+    }
+}
---
+++ Infrastructure/MiniEcommerceServer.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MiniEcommerceServer.Application.Repositories;
+using MiniEcommerceServer.Domain.Entities.Common;
+using MiniEcommerceServer.Persistence.Contexts;
+
+namespace MiniEcommerceServer.Persistence.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        private readonly ECommerceAPIContext _context;
+        public WriteRepository(ECommerceAPIContext context) => _context = context;
+
+        public DbSet<T> Table => _context.Set<T>();
+        public async Task<bool> AddAsync(T model)
+        {
+            EntityEntry<T> entityEntry = await Table.AddAsync(model);
+            return entityEntry.State == EntityState.Added;
+        }
+        public async Task<bool> AddRangeAsync(List<T> datas)
+        {
+            await Table.AddRangeAsync(datas);
+            return datas.All(m => _context.Entry(m).State == EntityState.Added);
+        }
+        public bool Remove(T model)
+        {
+            EntityEntry<T> entityEntry = Table.Remove(model);
+            return entityEntry.State == EntityState.Deleted;
+        }
+        public async Task<bool> RemoveAsync(string id)
+        {
+            T model = await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+            return Remove(model);
+        }
+        public bool RemoveRange(List<T> datas)
+        {
+            Table.RemoveRange(datas);
+            return datas.All(m => _context.Entry(m).State == EntityState.Deleted);
+        }
+        public async Task<int> SaveAsync()
+            => await _context.SaveChangesAsync();
+        public bool Update(T model)
+        {
+            EntityEntry<T> entityEntry = Table.Update(model);
+            return entityEntry.State == EntityState.Modified;
+        }
+    }
+}
