dfcacdb9e729f0f27e7a43562e61e38d3ff954f4
NihadRasulzada <nihadrs2005@gmail.com>
2025-03-13 17:54:28 +0400

    SaveChangeAsync Interceptor

--- Infrastructure/MiniEcommerceServer.Persistence/Contexts/ECommerceAPIContext.cs
+++ Infrastructure/MiniEcommerceServer.Persistence/Contexts/ECommerceAPIContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MiniEcommerceServer.Domain.Entities;
+using MiniEcommerceServer.Domain.Entities.Common;
 
 namespace MiniEcommerceServer.Persistence.Contexts
 {
@@ -11,5 +12,22 @@ namespace MiniEcommerceServer.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker
+                .Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow.AddHours(4),
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow.AddHours(4)
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
