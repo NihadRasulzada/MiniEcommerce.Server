4d0c20efa06cbc0bf6fd7dd4297a8e9a9a071b8e
NihadRasulzada <nihadrs2005@gmail.com>
2025-04-22 22:33:07 +0400

    SEO-Friendly Correction of File Names

--- Infrastructure/MiniEcommerceServer.Infrastructure/Services/FileService.cs
+++ Infrastructure/MiniEcommerceServer.Infrastructure/Services/FileService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using MiniEcommerceServer.Application.Services;
+using MiniEcommerceServer.Infrastructure.Operations;
 
 namespace MiniEcommerceServer.Infrastructure.Services
 {
@@ -18,7 +19,7 @@ namespace MiniEcommerceServer.Infrastructure.Services
             try
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
@@ -29,9 +30,57 @@ namespace MiniEcommerceServer.Infrastructure.Services
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
+            string newFileName = await Task.Run<string>(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string newFileName = String.Empty;
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+                }
+                else
+                {
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf("-");
+                    if (indexNo1 == -1)
+                        newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                    else
+                    {
+                        int lastIndex = 0;
+                        while (true)
+                        {
+                            lastIndex = indexNo1;
+                            indexNo1 = newFileName.IndexOf("-", indexNo1 + 1);
+                            if (indexNo1 == -1)
+                            {
+                                indexNo1 = lastIndex;
+                                break;
+                            }
+                        }
 
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo = newFileName.Substring(indexNo1 + 1, indexNo2 - indexNo1 - 1);
+
+                        if (int.TryParse(fileNo, out int _fileNo))
+                        {
+                            _fileNo++;
+                            newFileName = newFileName.Remove(indexNo1 + 1, indexNo2 - indexNo1 - 1)
+                                               .Insert(indexNo1 + 1, _fileNo.ToString());
+                        }
+                        else
+                            newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                    return await FileRenameAsync(path, newFileName, false);
+                else
+                    return newFileName;
+            });
+            return newFileName;
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -43,7 +92,7 @@ namespace MiniEcommerceServer.Infrastructure.Services
             List<bool> results = new();
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
                 results.Add(result);
